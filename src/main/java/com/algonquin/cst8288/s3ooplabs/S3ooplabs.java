50196ae2e308029ccc5b99d658c39334eee4d82f
RyanRen2023 <renxihai@gmail.com>
2024-05-28 21:12:03 -0400

    add main class

---
+++ src/main/java/com/algonquin/cst8288/s3ooplabs/S3ooplabs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.algonquin.cst8288.s3ooplabs;
+
+/**
+ *
+ * @author renxihai
+ */
+public class S3ooplabs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
